ff6ef7fc2c8c730faa4b099774f83bb51cc952c3
Nikhil <nikhilmrh123@gmail.com>
2023-07-21 19:49:19 +0530

    gft1

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -63,7 +63,8 @@ router.post("/login", async (req, res) => {
     }
 
     // create and assign a token
-    const token = jwt.sign({ userId: user._id }, 12347888, {
+    const secret=""
+    const token = jwt.sign({ userId: user._id }, secret+"secret", {
       expiresIn: "1d",
     });
     return res.send({
