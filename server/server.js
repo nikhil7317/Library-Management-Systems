1350c0d704e925a334b485e992547240b27593f6
Nikhil <nikhilmrh123@gmail.com>
2023-07-21 18:56:11 +0530

    server

--- server/server.js
+++ server/server.js
@@ -20,11 +20,11 @@ __dirname = path.resolve();
 
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static(path.join(__dirname, "/client/build")));
+  app.use(express.static(path.join(__dirname, "/build")));
 
   // index.html for all page routes
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "/build/index.html"));
   });
  }
 
