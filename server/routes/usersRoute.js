d4ba578bc5a0d05df6c31001f7df9c5f0da06817
Nikhil <nikhilmrh123@gmail.com>
2023-07-21 19:27:45 +0530

    secret

--- server/routes/usersRoute.js
+++ server/routes/usersRoute.js
@@ -63,7 +63,7 @@ router.post("/login", async (req, res) => {
     }
 
     // create and assign a token
-    const token = jwt.sign({ userId: user._id }, process.env.jwt_secret, {
+    const token = jwt.sign({ userId: user._id }, "323ed", {
       expiresIn: "1d",
     });
     return res.send({
